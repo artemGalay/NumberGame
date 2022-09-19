749347d7cd5b1351d56e330cd6be4ed73fa3009f
Artem Galay <iostema@mail.ru>
2022-09-20 00:40:54 +0300

    Delete main.

---
+++ NumberGame/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  NumberGame
+//
+//  Created by Артем Галай on 19.09.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
